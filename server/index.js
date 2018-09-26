2110bfed4184a90e96b905c746d122181ceb61c8
Brian Bycynski <high5bri@hotmail.com>
2018-09-26 07:34:21 -0400

    finished lecture 63

--- server/index.js
+++ server/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const config = require('./config/dev');
-const Rental = require('./models/rental');
 const FakeDb = require('./fake-db');
+const Rental = require('./models/rental');
+
+const rentalRoutes = require('./routes/rentals');
+
 mongoose
   .connect(
     config.DB_URI,
@@ -15,9 +18,7 @@ mongoose
 
 const app = express();
 
-app.get('/rentals', function(req, res) {
-  res.json({ success: true });
-});
+app.use('/api/v1/rentals', rentalRoutes);
 
 const PORT = process.env.PORT || 3001;
 
