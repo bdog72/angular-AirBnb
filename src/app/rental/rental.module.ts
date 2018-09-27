85a0b08589ce165918d85da4385f24b5c1d53b29
Brian Bycynski <high5bri@hotmail.com>
2018-09-27 05:51:43 -0400

    finished lecture 68

--- src/app/rental/rental.module.ts
+++ src/app/rental/rental.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { NgPipesModule } from 'ngx-pipes';
 
 import { RentalListComponent } from './rental-list/rental-list.component';
 import { RentalListItemComponent } from './rental-list-item/rental-list-item.component';
@@ -27,7 +29,12 @@ const routes: Routes = [
     RentalComponent,
     RentalDetailComponent
   ],
-  imports: [CommonModule, RouterModule.forChild(routes)],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+    HttpClientModule,
+    NgPipesModule
+  ],
   providers: [RentalService]
 })
 export class RentalModule {}
--- src/app/rental/shared/rental.service.ts
+++ src/app/rental/shared/rental.service.ts
@@ -1,82 +1,18 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-// import { setTimeout } from 'timers';
 import { Rental } from './rental.model';
+
+import { HttpClient } from '@angular/common/http';
 
 @Injectable()
 export class RentalService {
-  private rentals: Rental[] = [
-    {
-      id: '1',
-      title: 'Central Apartment',
-      city: 'New York',
-      street: 'Times Square',
-      category: 'apartment',
-      image: 'http://via.placeholder.com/350x250',
-      bedrooms: 3,
-      description: 'Very Nice Apartment',
-      dailyRate: 34,
-      shared: false,
-      createdAt: '24/12/2017'
-    },
-    {
-      id: '2',
-      title: 'Central Apartment 2',
-      city: 'San Francisco',
-      street: 'Main Street',
-      category: 'condo',
-      image: 'http://via.placeholder.com/350x250',
-      bedrooms: 2,
-      description: 'Very Nice Condo',
-      dailyRate: 12,
-      shared: true,
-      createdAt: '24/12/2017'
-    },
-    {
-      id: '3',
-      title: 'Central Apartment 3',
-      city: 'Bratsliva',
-      street: 'Hlavna',
-      category: 'condo',
-      image: 'http://via.placeholder.com/350x250',
-      bedrooms: 2,
-      description: 'Very Nice Condo',
-      dailyRate: 334,
-      shared: true,
-      createdAt: '24/12/2017'
-    },
-    {
-      id: '4',
-      title: 'Central Apartment 4',
-      city: 'Berlin',
-      street: 'Haupt Street',
-      category: 'house',
-      image: 'http://via.placeholder.com/350x250',
-      bedrooms: 3,
-      description: 'Very Nice House',
-      dailyRate: 33,
-      shared: true,
-      createdAt: '24/12/2017'
-    }
-  ];
+  constructor(private http: HttpClient) {}
 
-  public getRentalById(rentalId: string): Observable<Rental> {
-    return new Observable<Rental>(observer => {
-      setTimeout(() => {
-        const foundRental = this.rentals.find(rental => {
-          return rental.id === rentalId;
-        });
-
-        observer.next(foundRental);
-      }, 500);
-    });
+  public getRentalById(rentalId: string): Observable<any> {
+    return this.http.get('/api/v1/rentals/' + rentalId);
   }
 
-  public getRentals(): Observable<Rental[]> {
-    return new Observable<Rental[]>(observer => {
-      setTimeout(() => {
-        observer.next(this.rentals);
-      }, 1000);
-    });
+  public getRentals(): Observable<any> {
+    return this.http.get('/api/v1/rentals');
   }
 }
