874e5182d3d885044473515e72d888f231a08b32
Brian Bycynski <high5bri@hotmail.com>
2018-09-29 07:38:16 -0400

    finished lecture 85

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,10 +1,55 @@
+const User = require('../models/user');
+
 exports.auth = function(req, res) {};
 
 exports.register = function(req, res) {
-  const username = req.body.username;
-  const email = req.body.email;
-  const password = req.body.password;
-  const passwordConfirmation = req.body.passwordConfirmation;
+  const { username, email, password, passwordConfirmation } = req.body;
 
-  res.json({ username, email });
+  if (!username || !email) {
+    return res.status(422).send({
+      errors: [{ title: 'Data Missing!', detail: 'Provide email and password' }]
+    });
+  }
+
+  if (password !== passwordConfirmation) {
+    return res.status(422).send({
+      errors: [{ title: 'Invalid Password!', detail: 'Passwords dont match' }]
+    });
+  }
+
+  User.findOne({ email }, function(err, existingUser) {
+    if (err) {
+      return res.status(422).send({
+        mongoose: 'handle mongoose errors'
+      });
+    }
+
+    if (existingUser) {
+      return res.status(422).send({
+        errors: [
+          {
+            title: 'Invalid email!',
+            detail: 'User with this email already exists'
+          }
+        ]
+      });
+    }
+
+    const user = new User({
+      username,
+      email,
+      password
+    });
+
+    user.save(function(err) {
+      if (err) {
+        return res.status(422).send({
+          mongoose: 'handle mongoose errors'
+        });
+      }
+      return res.json({ registered: true });
+    });
+  });
+
+  // res.json({ username, email });
 };
