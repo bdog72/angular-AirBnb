f62809ceb844df105258e1d0c0afe5c822d8a4dd
Brian Bycynski <high5bri@hotmail.com>
2018-09-30 07:14:33 -0400

    finished lecture 89

--- server/models/user.js
+++ server/models/user.js
@@ -31,6 +31,10 @@ const userSchema = new Schema({
   ]
 });
 
+userSchema.methods.hasSamePassword = function(requestedPassword) {
+  return bcrypt.compareSync(requestedPassword, this.password);
+};
+
 userSchema.pre('save', function(next) {
   const user = this;
   bcrypt.genSalt(10, function(err, salt) {
