7a6a288b26c9ba3df21022746a234abad3272f09
Brian Bycynski <high5bri@hotmail.com>
2018-09-30 09:32:33 -0400

    finished lecture 92

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -89,3 +89,43 @@ exports.register = function(req, res) {
 
   // res.json({ username, email });
 };
+
+exports.authMiddleware = function(req, res, next) {
+  const token = req.headers.authorization;
+
+  if (token) {
+    const user = parseToken(token);
+
+    User.findById(user.userId, function(err, user) {
+      if (err) {
+        return res.status(422).send({ errors: normalizeErrors(err.errors) });
+      }
+
+      if (user) {
+        res.locals.user = user;
+        next();
+      } else {
+        return notAuthorized(res);
+      }
+    });
+  } else {
+    return notAuthorized(res);
+  }
+};
+
+function parseToken(token) {
+  'Bearer abcdefghijklmnop12345678';
+
+  return jwt.verify(token.split(' ')[1], config.SECRET);
+}
+
+function notAuthorized(res) {
+  return res.status(401).send({
+    errors: [
+      {
+        title: 'Not Authorized!',
+        detail: 'You need to lohin to get access'
+      }
+    ]
+  });
+}
--- server/fake-db.js
+++ server/fake-db.js
@@ -1,4 +1,5 @@
 const Rental = require('./models/rental');
+const User = require('./models/user');
 
 class FakeDb {
   constructor() {
@@ -40,23 +41,37 @@ class FakeDb {
         dailyRate: 23
       }
     ];
+    this.users = [
+      {
+        username: 'Test User',
+        email: 'test@gmail.com',
+        password: 'testtest'
+      }
+    ];
   }
 
   async cleanDb() {
     // await Rental.remove({});
+    await User.remove({});
     await Rental.deleteMany({});
   }
 
-  pushRentalsToDb() {
+  pushDataToDb() {
+    const user = new User(this.users[0]);
+
     this.rentals.forEach(rental => {
       const newRental = new Rental(rental);
+      newRental.user = user;
+
+      user.rentals.push(newRental);
 
       newRental.save();
     });
+    user.save();
   }
-  seeDb() {
-    this.cleanDb();
-    this.pushRentalsToDb();
+  async seeDb() {
+    await this.cleanDb();
+    this.pushDataToDb();
   }
 }
 module.exports = FakeDb;
