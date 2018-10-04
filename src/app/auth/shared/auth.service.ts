6362d4cdb91702286886f7a2e569fd95bc310efe
Brian Bycynski <high5bri@hotmail.com>
2018-10-04 13:00:50 -0400

    finished lecture 104

--- src/app/auth/shared/auth.service.ts
+++ src/app/auth/shared/auth.service.ts
@@ -1,12 +1,27 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+import 'rxjs/Rx';
 
 @Injectable()
 export class AuthService {
   constructor(private http: HttpClient) {}
 
   public register(userData: any): Observable<any> {
-    return this.http.post('api/v1/users/register', userData);
+    return this.http.post('/api/v1/users/register', userData);
+  }
+
+  public login(userData: any): Observable<any> {
+    return this.http
+      .post('/api/v1/users/auth', userData)
+      .map((token: string) => {
+        return this.saveToken(token);
+      });
+  }
+
+  private saveToken(token: string): string {
+    localStorage.setItem('bwm_auth', token);
+
+    return token;
   }
 }
