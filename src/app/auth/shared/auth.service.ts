cfccd3bb6d869b7a6bfd6b9da25d5aeff90ff9de
Brian Bycynski <high5bri@hotmail.com>
2018-10-05 05:36:13 -0400

    finished lecture 105

--- src/app/auth/shared/auth.service.ts
+++ src/app/auth/shared/auth.service.ts
@@ -1,10 +1,18 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+import * as jwt from 'jsonwebtoken';
 import 'rxjs/Rx';
+
+class DecodedToken {
+  exp: Number;
+  username: string = '';
+}
 
 @Injectable()
 export class AuthService {
+  private decodedToken;
+
   constructor(private http: HttpClient) {}
 
   public register(userData: any): Observable<any> {
@@ -20,7 +28,10 @@ export class AuthService {
   }
 
   private saveToken(token: string): string {
+    this.decodedToken = jwt.decode(token);
+
     localStorage.setItem('bwm_auth', token);
+    localStorage.setItem('bwm_meta', JSON.stringify(this.decodedToken));
 
     return token;
   }
