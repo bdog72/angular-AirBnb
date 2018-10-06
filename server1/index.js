91e56f47ac2a00ca13138f63f8f86a301f799331
Brian Bycynski <high5bri@hotmail.com>
2018-10-06 12:17:26 -0400

    having trouble with server, node section9

---
+++ server1/index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const config = require('./config/dev');
+const FakeDb = require('./fake-db');
+const Rental = require('./models/rental');
+
+const rentalRoutes = require('./routes/rentals');
+const userRoutes = require('./routes/users');
+
+mongoose
+  .connect(
+    config.DB_URI,
+    { useNewUrlParser: true }
+  )
+  .then(() => {
+    const fakeDb = new FakeDb();
+    // fakeDb.seeDb();
+  });
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use('/api/v1/rentals', rentalRoutes);
+app.use('/api/v1/users', userRoutes);
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(3001, function() {
+  console.log('I am running');
+});
