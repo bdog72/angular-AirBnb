73d99b53d20e3ff1c6d3a3aa45aa2eae3862ba9e
Brian Bycynski <high5bri@hotmail.com>
2018-10-09 16:02:34 -0400

    finished lecture 144

---
+++ src/app/rental/rental-create/rental-create.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { Rental } from '../shared/rental.model';
+
+import { RentalService } from '../shared/rental.service';
+
+@Component({
+  selector: 'bwm-rental-create',
+  templateUrl: './rental-create.component.html',
+  styleUrls: ['./rental-create.component.scss']
+})
+export class RentalCreateComponent implements OnInit {
+  newRental: Rental;
+  rentalCategories = Rental.CATEGORIES;
+
+  constructor(private rentalService: RentalService) {}
+
+  handleImageChange() {
+    this.newRental.image =
+      'https://booksync-jerga-prod.s3.amazonaws.com/uploads/rental/image/5/image.jpeg';
+  }
+
+  ngOnInit() {
+    this.newRental = new Rental();
+    this.newRental.shared = false;
+  }
+  createRental() {
+    this.rentalService
+      .createRental(this.newRental)
+      .subscribe(() => {}, () => {});
+  }
+}
--- src/app/rental/rental.module.ts
+++ src/app/rental/rental.module.ts
@@ -13,6 +13,7 @@ import { RentalListItemComponent } from './rental-list-item/rental-list-item.com
 import { RentalComponent } from './rental.component';
 import { RentalDetailComponent } from './rental-detail/rental-detail.component';
 import { RentalDetailBookingComponent } from './rental-detail/rental-detail-booking/rental-detail-booking.component';
+import { RentalSearchComponent } from './rental-search/rental-search.component';
 
 import { RentalService } from './shared/rental.service';
 import { BookingService } from '../booking/shared/booking.service';
@@ -20,6 +21,7 @@ import { HelperService } from '../common/service/helper.service';
 import { UppercasePipe } from '../common/pipes/uppercase.pipe';
 
 import { AuthGuard } from '../auth/shared/auth.guard';
+import { RentalCreateComponent } from './rental-create/rental-create.component';
 
 const routes: Routes = [
   {
@@ -28,9 +30,19 @@ const routes: Routes = [
     children: [
       { path: '', component: RentalListComponent },
       {
+        path: 'new',
+        component: RentalCreateComponent,
+        canActivate: [AuthGuard]
+      },
+
+      {
         path: ':rentalId',
         component: RentalDetailComponent,
         canActivate: [AuthGuard]
+      },
+      {
+        path: ':city/homes',
+        component: RentalSearchComponent
       }
     ]
   }
@@ -43,7 +55,9 @@ const routes: Routes = [
     RentalComponent,
     RentalDetailComponent,
     UppercasePipe,
-    RentalDetailBookingComponent
+    RentalDetailBookingComponent,
+    RentalSearchComponent,
+    RentalCreateComponent
   ],
   imports: [
     CommonModule,
