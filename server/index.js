44be7fe779e3342ed6b627ac85453f6d1cdc1d29
Brian Bycynski <high5bri@hotmail.com>
2018-10-11 15:00:55 -0400

    finished lecture 168

--- server/index.js
+++ server/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const config = require('./config/dev');
+const config = require('./config');
 const FakeDb = require('./fake-db');
 const Rental = require('./models/rental');
+const path = require('path');
 
 const rentalRoutes = require('./routes/rentals');
 const userRoutes = require('./routes/users');
@@ -15,8 +16,10 @@ mongoose
     { useNewUrlParser: true }
   )
   .then(() => {
-    const fakeDb = new FakeDb();
-    // fakeDb.seeDb();
+    if (process.env.NODE_ENV !== 'production') {
+      const fakeDb = new FakeDb();
+      // fakeDb.seeDb();
+    }
   });
 
 const app = express();
@@ -26,6 +29,15 @@ app.use(bodyParser.json());
 app.use('/api/v1/rentals', rentalRoutes);
 app.use('/api/v1/users', userRoutes);
 app.use('/api/v1/bookings', bookingRoutes);
+
+if (process.env.NODE_ENV === 'production') {
+  const appPath = path.join(__dirname, '..', 'dist');
+  app.use(express.static(appPath));
+
+  app.get('*', function(req, res) {
+    res.sendFile(path.resolve(appPath, 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 3001;
 
