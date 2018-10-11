2de9c7a2ba264c7be4b399d0c4e983f0674b4998
Brian Bycynski <high5bri@hotmail.com>
2018-10-11 07:53:32 -0400

    finished lecture 158

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { HeaderComponent } from './common/header/header.component';
 
 import { RentalModule } from './rental/rental.module';
 import { AuthModule } from './auth/auth.module';
+import { ManageModule } from './manage/manage.module';
 
 const routes: Routes = [
   { path: '', redirectTo: '/rentals', pathMatch: 'full' }
@@ -25,7 +26,8 @@ const routes: Routes = [
     AuthModule,
     NgbModule.forRoot(),
     ToastModule.forRoot(),
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    ManageModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/manage/manage-rental/manage-rental.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { RentalService } from '../../rental/shared/rental.service';
+import { Rental } from '../../rental/shared/rental.model';
+
+@Component({
+  selector: 'bwm-manage-rental',
+  templateUrl: './manage-rental.component.html',
+  styleUrls: ['./manage-rental.component.scss']
+})
+export class ManageRentalComponent implements OnInit {
+  rentals: Rental[];
+
+  constructor(private rentalService: RentalService) {}
+
+  ngOnInit() {
+    this.rentalService.getUserRentals().subscribe(
+      (rentals: Rental[]) => {
+        this.rentals = rentals;
+      },
+      () => {}
+    );
+  }
+}
